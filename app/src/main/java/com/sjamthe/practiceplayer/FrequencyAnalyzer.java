91e5abd0e538ee4968b709bda1a2b5377e869968
sjamthe <sjamthe@yahoo.com>
2022-12-06 19:07:43 -0800

    Added testcases for FFT

--- app/src/main/java/com/sjamthe/practiceplayer/FrequencyAnalyzer.java
+++ app/src/main/java/com/sjamthe/practiceplayer/FrequencyAnalyzer.java
@@ -2,11 +2,26 @@ package com.sjamthe.practiceplayer;
 
 import android.util.Log;
 
+/*
+Find Pitch using ACF (Auto Correlation Function
+Ref:
+https://interactiveaudiolab.github.io/teaching/eecs352stuff/CS352-Single-Pitch-Detection.pdf
+https://ccrma.stanford.edu/~pdelac/PitchDetection/icmc01-pitch.pdf
+https://www.ijert.org/research/robust-pitch-detection-algorithm-of-pathological-speech-based-on-acf-and-amdf-IJERTV4IS050380.pdf
+https://www.dafx.de/paper-archive/2019/DAFx2019_paper_54.pdf
+https://gist.github.com/ajkluber/f293eefba2f946f47bfa
+https://otexts.com/fpp2/autocorrelation.html
+https://www.ymec.com/hp/signal2/acf.htm
+ */
+
 public class FrequencyAnalyzer {
-    private static final double FREQ_A3 = Math.pow(2.0d, 0.0d) * 220.0d;
-    private static final double FREQ_A3_SHARP = Math.pow(2.0d, 0.08333333333333333d) * 220.0d;
+    public static final double FREQ_A1 = 55.0d;
+    public static final double SEMITONE_INTERVAL = Math.pow(2.0d, 1.0d/12.0d);
+    // Example: to calculate any freq like C3 will be 12+3 semitones from A1 or 130.8Hz
+    public static final double FREQ_C3 = FREQ_A1*Math.pow(SEMITONE_INTERVAL,(12.0d+3.0d));
+    public static final double SAMPLING_SIZE = 44100.0d;
     public static final int FFT_SIZE = (int) Math.pow(2.0d,
-            ((int) log2(44100.0d / (FREQ_A3_SHARP - FREQ_A3))) + 1);
+            ((int) log2(SAMPLING_SIZE / (FREQ_A1*(SEMITONE_INTERVAL-1)))) + 1);
 
     public static double log2(double d) {
         return Math.log(d) / Math.log(2.0d);
--- app/src/test/java/com/sjamthe/practiceplayer/FFT4gTest.java
+++ app/src/test/java/com/sjamthe/practiceplayer/FFT4gTest.java
@@ -1,6 +1,9 @@
 package com.sjamthe.practiceplayer;
 
 import static com.sjamthe.practiceplayer.FrequencyAnalyzer.FFT_SIZE;
+import static com.sjamthe.practiceplayer.FrequencyAnalyzer.SAMPLING_SIZE;
+import static com.sjamthe.practiceplayer.FrequencyAnalyzer.FREQ_A1;
+import static com.sjamthe.practiceplayer.FrequencyAnalyzer.SEMITONE_INTERVAL;
 
 import junit.framework.TestCase;
 
@@ -10,46 +13,114 @@ import org.junit.Test;
 
 public class FFT4gTest {
 
-    double[] a = new double[FFT_SIZE];
     double[] signal = new double[FFT_SIZE];
     FFT4g fft;
 
     @Before
     public void setUp() throws Exception {
+        fft = new FFT4g(FFT_SIZE);
+    }
 
+    private double[] generateRandomSample() {
+        double[] a = new double[FFT_SIZE];
         int seed = 0;
         for (int i=0; i < a.length; i++) {
             a[i] = (seed * (1.0 / 259200.0));
-            signal[i] = a[i]; // keep backup to compare output
             seed = ((seed * 7141 + 54773) % 259200);
         }
-
-        fft = new FFT4g(FFT_SIZE);
-
+        return a;
     }
 
-    @Test
-    public void setup_isCorrect() {
-        Assert.assertEquals(FFT_SIZE, a.length);
+    private static double[] generateSineWaveFreq(double frequencyOfSignal) {
+        double[] sin = new double[FFT_SIZE];
+        double samplingInterval = (double) (SAMPLING_SIZE / frequencyOfSignal);
+        for (int i = 0; i < sin.length; i++) {
+            double angle = (2.0 * Math.PI * i) / samplingInterval;
+            sin[i] = Math.sin(angle);
+        }
+        return sin;
     }
 
-    @Test
-    public void rdft_works() {
+    private double compareArrays(double []a, double []b) {
         double max_err = 0;
-        fft.rdft(1, a);
-        fft.rdft(-1, a);
-        Assert.assertEquals(FFT_SIZE, a.length);
-        double scale = 2.0/FFT_SIZE;
-
+        // Compare IFFT results with input
+        double scale = 2.0/a.length;
         for (int j=0; j<a.length; j++) {
-            double err = signal[j] - a[j] * scale;
+            double err = b[j] - a[j] * scale;
             err = Math.abs(err);
             max_err = ((max_err) > (err) ? (max_err) : (err));
             if(max_err > 0.000000001) {
                 System.out.printf("Error At %d", j);
             }
         }
-        System.out.printf("Max Error %g", max_err);
+        return max_err;
+    }
+
+    @Test
+    public void setup_isCorrect() {
+        double[] a = generateRandomSample();
+        Assert.assertEquals(FFT_SIZE, a.length);
+    }
+
+    private int largestIndex(double[] arr)
+    {
+        int i;
+
+        // Initialize maximum element
+        double max = arr[0];
+        int maxItem = -1;
+
+        for (i = 0; i < arr.length/2; i++) {
+            double val = Math.sqrt(arr[2 * i] * arr[2 * i] + arr[2 * i + 1] * arr[2 * i + 1]);
+            if (val > max) {
+                max = val;
+                maxItem = i;
+            }
+        }
+        return maxItem;
+    }
+
+    @Test
+    public void rdft_RandomSample() {
+        double[] a = generateRandomSample();
+        signal = a.clone(); // keep backup to compare with IFFT output
+        fft.rdft(1, a); // FFT
+        fft.rdft(-1, a); // IFFT
+        Assert.assertEquals(FFT_SIZE, a.length);
+
+        double max_err = compareArrays(a, signal);
+        System.out.printf("Max Error %g\n", max_err);
         Assert.assertTrue(max_err < 0.000000001);
+    }
+
+    private static double Index2Freq(int i, double samples, int nFFT) {
+        return (double) i * (samples / nFFT);
+    }
+
+    private static int Freq2Index(double freq, double samples, int nFFT) {
+        return (int) (freq / (samples / nFFT));
+    }
+
+    @Test
+    public void rdft_sineWave() {
+
+        double fftBandSize = SAMPLING_SIZE/FFT_SIZE;
+
+        for (int i=0; i<12*8; i++) {
+            double testFreq = FREQ_A1/2 * Math.pow(SEMITONE_INTERVAL, i);
+            double[] a = generateSineWaveFreq(testFreq);
+            signal = a.clone(); // keep backup to compare with IFFT output
+            fft.rdft(1, a); // FFT
+            int maxIndex = largestIndex(a);
+            double freq = Index2Freq(maxIndex, SAMPLING_SIZE, FFT_SIZE);
+            System.out.printf("testFreq = %.1f, outputFreq = %.1f\n", testFreq, freq);
+
+            //Validate that the predicted freq is within FFT band size.
+            Assert.assertTrue(Math.abs(freq - testFreq) < fftBandSize);
+
+            fft.rdft(-1, a); // IFFT
+            double max_err = compareArrays(a, signal);
+            Assert.assertTrue(max_err < 0.000000001);
+        }
     }
 }
